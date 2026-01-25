8f7391c119a33b4f62cf95b38cfc71d60e7161e6
Dinavel Binongo <binongo.dinavel.escodero@gmail.com>
2026-01-25 18:01:42 +0800

    .

--- mango-app/src/App.tsx
+++ mango-app/src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <>
-      <h1>test</h1>
+      <Homepage />
     </>
   );
 }
