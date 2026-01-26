010ba9ff586a573aa26c01db9199c528263148a3
dinavelB <binongo.dinavel.escodero@gmail.com>
2026-01-26 16:58:12 +0800

    push

--- mango-app/src/components/Features.tsx
+++ mango-app/src/components/Features.tsx
@@ -3,16 +3,16 @@ import features from "../util/features";
 export default function Feature() {
   return (
     <>
-      <section>
-        <section className="">
-          <h1>Features</h1>
+      <section className="flex justify-center pt-20">
+        <section className=" bg-white w-[1700px] rounded-2xl overflow-hidden p-10">
+          <h1 className=" text-3xl font-extrabold">Features</h1>
           {features.map((feature, index) => (
-            <div key={index}>
-              <div>
-                <img src={feature.img} alt="" />
-                <h1>{feature.title}</h1>
+            <div key={index} className="pt-20">
+              <div className="flex items-center">
+                <img className=" object-cover" src={feature.img} alt="" />
+                <h1 className="">{feature.title}</h1>
               </div>
-              <p>{feature.description}</p>
+              <p className="pt-20 shadow-borderColor ">{feature.description}</p>
             </div>
           ))}
         </section>
