3067761d485d544f6356db355d43f11d170695da
Dinavel Binongo <binongo.dinavel.escodero@gmail.com>
2026-01-27 11:33:31 +0800

    footer

--- mango-app/src/pages/Homepage.tsx
+++ mango-app/src/pages/Homepage.tsx
@@ -1,13 +1,15 @@
 import NavBar from "../components/NavBar";
 import HeroSection from "../components/HeroSection";
 import Feature from "../components/Features";
+import Footer from "../components/Footer";
 
 export default function Homepage() {
   return (
     <>
       <NavBar />
       <HeroSection />
-      <Feature/>
+      <Feature />
+      <Footer />
     </>
   );
 }
