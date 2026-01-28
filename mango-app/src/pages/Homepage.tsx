f7a511c5b8fde492f27a2c00ef4bef1439957876
Glenn Mendoza <64859540+GlennBMendoza@users.noreply.github.com>
2026-01-28 09:50:30 +0800

    removed unused method

--- mango-app/src/pages/Homepage.tsx
+++ mango-app/src/pages/Homepage.tsx
@@ -2,7 +2,6 @@ import NavBar from "../components/NavBar";
 import HeroSection from "../components/HeroSection";
 import Feature from "../components/Features";
 import Footer from "../components/Footer";
-import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 export default function Homepage() {
