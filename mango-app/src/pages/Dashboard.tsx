655c5767c1c676b42adcdf72720253511352598e
dinavelB <binongo.dinavel.escodero@gmail.com>
2026-03-09 11:07:53 +0800

    installed shadcn

--- mango-app/src/pages/Dashboard.tsx
+++ mango-app/src/pages/Dashboard.tsx
@@ -3,8 +3,6 @@ import Nav from "../components/Nav";
 export default function Login() {
   return (
     <section className=" bg-pattern-dot min-h-screen">
-      <Nav />
-      <h1>this is a test</h1>
     </section>
   );
 }
